b187a55ead4f2eef8b812184221af29a087fca84
Jcalise <julien.calise@orange.fr>
2020-06-19 12:18:36 +0200

    Menu_Ok

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function myMenu() {
+    var x = document.getElementById("monMenu");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
